58ae4639a33bc6bf5c9369582e0173fd024a1b8d
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-20 17:38:10 +0300

    Initial Commit

---
+++ WalletApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletApp
+//
+//  Created by Yunus Emre Taşdemir on 20.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletApp/WalletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppApp.swift
+//  WalletApp
+//
+//  Created by Yunus Emre Taşdemir on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
